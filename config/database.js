5c29ac88cc90dbe6ab8f07b054cd8ac29ab6cef2
KhachikHarutyunyan <khachik789@gmail.com>
2018-05-17 17:05:35 +0400

    test db

--- config/database.js
+++ config/database.js
@@ -3,8 +3,8 @@ const crypto = require('crypto').randomBytes(256).toString('hex'); // Provides c
 // Export config object
 module.exports = {
   // uri: 'mongodb://localhost:27017/mean-angular-2', // Databse URI and database name
-  uri: 'mongodb://test:@Aa123456@ds139459.mlab.com:39459/angular-2-app', // Databse URI production
+  uri: 'mongodb://test:test@ds227110.mlab.com:27110/mean-blog', // Databse URI production
   secret: crypto, // Cryto-created secret- crypto modul shifrovania
   // db: 'mean-angular-2' // Database name
-  db: 'angular-2-app' // Database name from mLab
+  db: 'mean-blog' // Database name from mLab
 }
